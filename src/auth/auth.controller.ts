15fb616c834dce734329197a7ace7c63db8b2e65
cbcristhian <cristhian.castro@unosquare.com>
2024-06-30 12:29:01 -0400

    MongoDb Connected to AuthMS

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,22 +1,25 @@
 import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { RegisterUserDto } from './dto/register-user.dto';
+
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @MessagePattern('auth.register.user')
-  create(@Payload() createAuthDto) {
-    return this.authService.create(createAuthDto);
+  registerUser(@Payload() registerUser:RegisterUserDto) {
+    return this.authService.create(registerUser);
   }
 
   @MessagePattern('auth.login.user')
-  findAll() {
-    return this.authService.findAll();
+  loginUser(@Payload() loginDto:LoginDto) {
+    return loginDto
   }
 
   @MessagePattern('auth.verify.user')
-  findOne(@Payload() id: number) {
+  verifyToken(@Payload() id: number) {
     return this.authService.findOne(id);
   }
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit, Logger } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class AuthService {
+export class AuthService extends PrismaClient implements OnModuleInit{
+
+  private readonly logger = new Logger("AuthService")
+
+  onModuleInit() {
+    this.$connect();
+    this.logger.log('Auth MS DB Connected')
+  }
   create(createAuthDto) {
     return 'This action adds a new auth';
   }
